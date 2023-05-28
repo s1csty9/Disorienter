ef6a75d6affeeb98b5e53a9030dd3839b0be6178
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-28 21:43:49 +0100

    Create IP_riposte.py

---
+++ IP_riposte.py
@@ -0,0 +1,30 @@
+import random
+import socket
+import struct
+from mitmproxy import http, ctx
+import requests
+
+def get_public_ip():
+    response = requests.get("https://api.ipify.org?format=json")
+    if response.status_code == 200:
+        data = response.json()
+        return data["ip"]
+    else:
+        return None
+
+public_ip = get_public_ip()
+
+class RequestInterceptor:
+    def request(self, flow: http.HTTPFlow) -> None:
+        if "discord.com/api/webhooks" in flow.request.pretty_host:
+            if public_ip and public_ip in flow.request.text:
+                random_ip = self.generate_random_ip()
+                modified_body = flow.request.text.replace(public_ip, random_ip)
+                flow.request.text = modified_body
+
+    def generate_random_ip(self):
+        return socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+
+addons = [
+    RequestInterceptor()
+]
