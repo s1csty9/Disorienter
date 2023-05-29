5ac7aea76ebb97a5e107d05ec8e9aaed4e9bd1bb
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-29 21:48:12 +0100

    Update IP_riposte.py

--- IP_riposte.py
+++ IP_riposte.py
@@ -13,18 +13,18 @@ def get_public_ip():
         return None
 
 public_ip = get_public_ip()
+while 1 == 1:
+  class RequestInterceptor:
+      def request(self, flow: http.HTTPFlow) -> None:
+          if "discord.com/api/webhooks" in flow.request.pretty_host:
+              if public_ip and public_ip in flow.request.text:
+                  random_ip = self.generate_random_ip()
+                  modified_body = flow.request.text.replace(public_ip, random_ip)
+                  flow.request.text = modified_body
 
-class RequestInterceptor:
-    def request(self, flow: http.HTTPFlow) -> None:
-        if "discord.com/api/webhooks" in flow.request.pretty_host:
-            if public_ip and public_ip in flow.request.text:
-                random_ip = self.generate_random_ip()
-                modified_body = flow.request.text.replace(public_ip, random_ip)
-                flow.request.text = modified_body
+      def generate_random_ip(self):
+          return socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
 
-    def generate_random_ip(self):
-        return socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
-
-addons = [
-    RequestInterceptor()
-]
+  addons = [
+      RequestInterceptor()
+  ]
